9163d8c0198a620d3cd8898881635857bb8548f2
Jimmy.ocw <chengwei.cw.ou@gtomato.com>
2022-02-22 18:20:45 +0800

    move var config to google_cloud

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM python:3.7.8
 
 WORKDIR /
-EXPOSE 8000
 COPY . ./
 
 RUN pip install -r dependencies.txt
--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,6 @@
-echo "[STEP 1] begin to migrate any thing"
+echo "[STEP 1] begin to migrate anything"
 python3 ./manage.py makemigrations
 python3 ./manage.py migrate
 
-echo "[STEP 2] run server"
-python3 ./manage.py runserver 0.0.0.0:8000
+echo "[STEP 2] begin to run server, will listening at port:$DJANGO_PORT"
+python3 ./manage.py runserver 0.0.0.0:$DJANGO_PORT
