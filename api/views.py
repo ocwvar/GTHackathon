4071a411a7213ae3081fe9018c5076bbc930480c
Jimmy.ocw <chengwei.cw.ou@gtomato.com>
2022-02-24 14:09:06 +0800

    hhh

--- api/views.py
+++ api/views.py
@@ -10,7 +10,7 @@ class RequestEvent(APIView):
         movement_code = self.request.GET.get("movement", "F")
         saved_list = SavedData.objects.filter(code="ME")
 
-        if saved_list.count() == 0 :
+        if saved_list.count() == 0:
             new_object = SavedData(code="ME", move=movement_code)
             new_object.save()
         else:
@@ -22,9 +22,14 @@ class RequestEvent(APIView):
 
     def post(self, request, *args, **kwargs):
         saved_list = SavedData.objects.filter(code="ME")
-        if saved_list.count() == 0 :
-            return HttpResponse("F")
+        if saved_list.count() == 0:
+            return HttpResponse("T")
         else:
             save_object = saved_list[0]
-            return HttpResponse(save_object.move)
-    
+            if save_object.move == 'L' or save_object.move == 'R':
+                value = save_object.move
+                save_object.move = "T"
+                save_object.save(update_fields=["move"])
+                return HttpResponse(value)
+            else:
+                return HttpResponse(save_object.move)
